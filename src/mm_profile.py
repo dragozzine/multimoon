192617c10ba573638379fe6d8107bf6aba4a2b1e
Dallin Spencer <dallinspencer@gmail.com>
2020-06-19 14:02:57 -0600

    Updated profile tests

--- src/mm_profile.py
+++ src/mm_profile.py
@@ -9,9 +9,16 @@ pr.enable()
 import mm_run
 
 pr.disable()
-s = io.StringIO()
-ps = pstats.Stats(pr, stream=s).sort_stats('cumtime')
-ps.print_stats()
+s1 = io.StringIO()
+ps1 = pstats.Stats(pr, stream=s1).sort_stats('cumtime')
+ps1.print_stats()
 
-with open('test.txt', 'w+') as f:
-    f.write(s.getvalue())
+with open('test_cum.txt', 'w+') as f:
+    f.write(s1.getvalue())
+
+s2 = io.StringIO()    
+ps2 = pstats.Stats(pr, stream=s2).sort_stats('tottime')
+ps2.print_stats()
+
+with open('test_tot.txt', 'w+') as f:
+    f.write(s2.getvalue())
