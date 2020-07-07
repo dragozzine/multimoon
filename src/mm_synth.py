3f4736da623be46cdb0fa81e84d72014bc216767
Benjamin Proudfoot <benp175@gmail.com>
2020-07-07 11:56:15 -0600

    more work on synthetic astrometry

--- src/mm_synth.py
+++ src/mm_synth.py
@@ -7,6 +7,13 @@
 #	06/19/20
 #
 
+class ReadJson(object):
+    def __init__(self, filename):
+        print('Read the runprops.txt file')
+        self.data = json.load(open(filename))
+    def outProps(self):
+        return self.data
+
 # I think the best method would be to have a tests directory where we store the synthetic astrometry,
 # an output file associated with the creation of that synthetic astrometry (basically the parameters),
 # and everything else needed to test the system (geocentric_obj_position.csv, etc).
@@ -15,15 +22,44 @@
 
 # load in all required packages and functions
 import numpy as np
-.
-.
-.
+import pandas as pd
+import sys
+import h5py
+import random
+import json
+
+import mm_priors as prior
+import mm_likelihood
+import mm_param
+
+sys.path.insert(1, 'mm_SPINNY/')
+from mm_SPINNY.spinny_vector import generate_vector
+import mm_relast
 
 # maybe have a runprops type thing to hold the inputs?
+# it then saves a copy of this to the directory where the test case is stored
 
-# change mm_chisquare to have an output option which just outputs the Model_DeltaLong and 
-# Model_DeltaLat
-# This will contain all of the data relevant to creating the synthetic astrometry
+# Getting the inputs from runprops_gensynth.txt
+# This file needs to have everything that mm_chisquare needs
+#	numobjects, verbose, float_dict, 
+filename = "runprops_gensynth.txt"
+
+getData= ReadJson(filename)
+runprops = getData.outProps()
+
+# Setting the observations data file
+runprops["obsdata_file"] = 
+
+# Package the parameters wanted into a guesses-like df
+
+# Creating a paramdf
+float_params,float_names,fixed_df,total_df_names,fit_scale = mm_param.from_param_df_to_fit_array(guesses,runprops)
+name_dict = runprops.get("names_dict")
+paramdf = mm_param.from_fit_array_to_param_df(float_params, float_names, fixed_df, total_df_names, fit_scale, name_dict)
+
+# Outputting model astrometry based on the params df
+Model_DeltaLong, Model_DeltaLat = mm_chisquare(paramdf, obdsf, runprops, gensynth = True)
+
 
 # Need to think about excluding data points where the secondary/tertiary/etc is aligned with the 
 # primary. In theory the fitter will have no problem fitting to this data, but it really isn't 
