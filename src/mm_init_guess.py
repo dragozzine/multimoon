ceff92318b585c7e6b6f7b3d3e0d2f57f7d9923b
Ben Proudfoot <benp175@gmail.com>
2020-10-10 11:51:14 -0600

    Bug fixes

--- src/mm_init_guess.py
+++ src/mm_init_guess.py
@@ -47,9 +47,11 @@ def mm_init_guess(runprops):
                 cut_df[col] = start_guess_df[col]
         
 
-    for col in cut_df.columns:
+#    for col in cut_df.columns:
+    for col in start_guess_df.columns:
         name_dict[n] = col
-        infos = cut_df[col].to_numpy()
+        #infos = cut_df[col].to_numpy()
+        infos = start_guess_df[col].to_numpy()
         mean1, stdev1 = infos[0],infos[1]
         #print(col)
         #print(fix_float.get(col))
