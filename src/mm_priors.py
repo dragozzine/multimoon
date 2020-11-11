606eff3a92e012d8d86f5c7e4412bb8a6707e2f4
Dallin Spencer <dallinspencer@gmail.com>
2020-11-10 20:52:26 -0700

    Fixed min_periapse

--- src/mm_priors.py
+++ src/mm_priors.py
@@ -123,7 +123,9 @@ def mm_priors(priors, parameters, runprops):
     # Making sure min periapse is obeyed
     min_periapse = runprops.get("min_periapse")
     for i in range(1,runprops.get("numobjects")):
-        if (params["sma_" + str(i+1)].values[0]*params["ecc_" + str(i+1)].values[0] < min_periapse.get('sat_'+str(i)):
+        if i == 1 and (params["sma_" + str(i+1)].values[0]*(1-params["ecc_" + str(i+1)].values[0]) < min_periapse):
+            return -np.inf
+        elif i != 1 and (params["sma_" + str(i+1)].values[0]*(1-params["ecc_" + str(i+1)].values[0])-params["sma_" + str(i)].values[0]*(1+params["ecc_" + str(i)].values[0]) < min_periapse):
             return -np.inf
 
     if runprops.get('verbose'):
