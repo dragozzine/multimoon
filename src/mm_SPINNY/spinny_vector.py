10baa48c3ac15a283369b6f3f4258a2a7eea797e
Benjamin Proudfoot <benp175@gmail.com>
2022-05-05 11:03:20 -0600

    cleaning spinny

--- src/mm_SPINNY/spinny_vector.py
+++ src/mm_SPINNY/spinny_vector.py
@@ -22,9 +22,6 @@ def generate_vector(paramsdf, t_arr, runprops):
     N = runprops.get("numobjects") # total number of objects in the system
     T = len(t_arr)                 # number of observation times
 
-    # Edit made by BP on 07/08/20: Please tell me if this breaks something!
-    #t_arr = t_arr - t_arr[0]
-
     j2s = []
     for col in sys_df.columns:
         if 'j2r2' in col:
@@ -42,12 +39,9 @@ def generate_vector(paramsdf, t_arr, runprops):
     
     #print('sys_Df', sys_df)
     if N == 2 and j2_sum == 0.00 and not includesun:  # checks if all objects are point masses, does keplerian integration instead
-        #print('sys_Df:', sys_df)
         kepler_system = kepler_2body(sys_df,t_arr, runprops)
-        #print('got the kepler system')
         s_df = kepler_system[0]
         names = kepler_system[1]
-        #print('next')
         
     elif mass_sum == mass_primary: #checks if only the primary has mass, all other bodies are massless
         kepler_system = kepler_nbody(sys_df,t_arr, runprops)
@@ -56,28 +50,18 @@ def generate_vector(paramsdf, t_arr, runprops):
 
     elif not includesun and N >= 2: # runs a SPINNY integration without the sun if not included 
         system = build_spinny_multimoon(sys_df, runprops)
-        #print("spinny_vector line 54")
         spinny = evolve_spinny_ns(system[0],system[1],system[2],system[3],system[4],system[5],t_arr,tol, runprops)
-        #print("spinny_vector line 56")
         s_df = spinny[0]
         names = spinny[2]
         
     elif includesun and N >= 2: # runs SPINNY with the sun included
-        #print('vector line 66')
-        #print('sys_df line 67', sys_df)
         system = build_spinny_multimoon(sys_df, runprops)
-        #print('vector line 68')
         spinny = evolve_spinny(system[0],system[1],system[2],system[3],system[4],system[5],t_arr, runprops)
         s_df = spinny[0]
-        #print('s_df 72', s_df)
         names = spinny[1]
-        #names = np.delete(names,0)
-        #print(names)
-        #print('Done')
 
       
     # creates a new dataframe using x,y,z position and velocity for each body
-    #print('times arr')
     data = {"Times":t_arr}
 
     for name in names:
@@ -87,7 +71,6 @@ def generate_vector(paramsdf, t_arr, runprops):
         data.setdefault("X_Vel_"+name, s_df["X_Vel_"+name])
         data.setdefault("Y_Vel_"+name, s_df["Y_Vel_"+name])
         data.setdefault("Z_Vel_"+name, s_df["Z_Vel_"+name])
-    #print('spiny vector line 85')
     vec_df = pd.DataFrame(data)
     return(vec_df)
             
@@ -120,6 +103,7 @@ def build_spinny_multimoon(sys_df, runprops):
     #NOTE: as long as the runprops dict is sorted in order of descending mass, this should not be needed
     #cols = list(sys_df.columns[[int(np.where(sys_df==m)[1].flatten()) for m in masses]])
     #body_idx = [int(body[-1]) for body in cols]
+    # leaving this just in case...
 
     names_arr = np.empty(N,dtype="object")
     phys_arr = np.zeros((N,4))
@@ -133,14 +117,11 @@ def build_spinny_multimoon(sys_df, runprops):
         names_arr[n] = name_n
     
     i = 0
-    #print('sys_df line 128 vector', sys_df)
     if runprops.get('includesun') == 1:
-        #names_arr[0] = 'Sun'
         N = N-1
         begin = 0
     else:
         begin = 1
-    #print(begin, N+1)
     if runprops.get('verbose'):
         print('vector line 145')
         
@@ -178,10 +159,8 @@ def build_spinny_multimoon(sys_df, runprops):
                 axes = runprops.get('axes_size')
                 ax_n = axes['obj_'+str(n)]
                 
-                #ax_n = runprops.get("c_axis")
                 if (ax_n == None):
                     print("\n\n\033[1;37;41mRunprops has been updated to require a value for the primary axis. Please include this before proceeding. please include an axis in runprops for object ", str(n)," to a value. Aborting run.\033[0m\n\n")
-                    #ax_n = 1
                     sys.exit()
                 
         # set physical properties array
@@ -225,7 +204,6 @@ def build_spinny_multimoon(sys_df, runprops):
         if runprops.get('verbose'):
             print('vector line 222')
     # set orbit of primary equal to orbit of secondary (it will be scaled later)
-    #print(orb_arr)
     if "Sun" in names_arr:
         orb_arr[1] = orb_arr[2]
         
@@ -234,7 +212,6 @@ def build_spinny_multimoon(sys_df, runprops):
         
     
     i = 1
-    #print('line 225')
     for n in range(1,N+1):
         # precession, obliquity angles are measured with respect to the ECLIPTIC, not the body's orbit
         # default values are set to be aligned with the orbit (LAN for prec, inc for obliq, AOP for longitude)
@@ -260,12 +237,10 @@ def build_spinny_multimoon(sys_df, runprops):
         else:
             # the default is equal to the orbital period, calculated from semi-major axis, sum of masses of body and primary 
             #sp_rate_n = 2*np.pi/np.sqrt(orb_arr[i,0]**3.0/(G*(phys_arr[i,0]+phys_arr[1,0])) )
+            # TODO: does this make a difference?? I don't think so since spins arent calculated when no spins
             sp_rate_n = 0
-            #print(sp_rate_n)
 
         # set spin properties array
-        
-        #print('line 254')
         if runprops.get('verbose'):
             print('vector line 265')
         spin_arr[n-1] = np.array([sp_prc_n, sp_obl_n, sp_lon_n, sp_rate_n])
@@ -287,13 +262,11 @@ def build_spinny_multimoon(sys_df, runprops):
         qr = quat_i[3]
         # set quaternion array
         # the order of the quaternion must be changed, since SPINNY needs scalar-first form
-        #print('line 271')
         if runprops.get('verbose'):
             print('vector line 287')
         quat_arr[n-1] = np.array([qr,qi,qj,qk]) #obliq_quat_n[0]
 
         i = i+1
-    #print('line 272')  
     if runprops.get('includesun'):
         N = N+1
     if runprops.get('verbose'):
@@ -375,10 +348,6 @@ def spinny_output(sys_df,t_start,t_end, runprops):
 
 def spinny_output_multiple(sys_df,t_start,t_end, runprops): #sys_df should have multiple rows with different parameters
     
-    #sys_df = pd.read_csv("../data/spinny_test_data/mm_multiple_test.csv")
-    #t_start = 0.
-    #t_end = 60.*24.*3600.0
-    
     num_rows = len(sys_df.index) # total number of rows/models to generate
     t_arr = np.linspace(t_start,t_end,2000)
     tol = runprops.get("spinny_tolerance")
@@ -400,9 +369,3 @@ def spinny_output_multiple(sys_df,t_start,t_end, runprops): #sys_df should have 
         
     spinny_plot_multiple(df_list,names,t_arr)
 
-    
-    
-    
-    
-
-        
